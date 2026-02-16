53ce30842be1512b89c4a6f1cb43cac4c79ceed2
sruthilakkakula13 <sruthilakkakula13@gmail.com>
2026-02-16 16:21:11 +0530

    Fixed merge conflict in Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins + FPM + DEB!");
+        System.out.println("Hello World");
     }
-=======
-public class Hello{
-public static void main(String[] args){
-System.out.println("Hello from jenkins + FPM + DEB!");
 }
->>>>>>> 202559ff7dfd45bc223c903c1502c93476fe5e1c
-}
+
